28d46c623f09b49a3fb2f95016a6a83792fd40e7
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-14 13:53:19 -0700

    Lists names as a string

--- src/order.js
+++ src/order.js
@@ -9,11 +9,20 @@ function refundOrder(orderNumber, deliveryOrder) {
     const index = deliveryOrder.findIndex((order) => order.orderNumber === orderNumber);
     
     if (index !== -1) {
-        return deliveryOrder.splice(index, 1)
+        return deliveryOrder.splice(index, 1);
     }
+}
+
+function listItems(deliveryOrders) {
+    var names = '';
+
+    deliveryOrders.forEach((order) => names += `${order.item}, `);
+
+    return names.slice(0, -2);
 }
 
 module.exports = {
     takeOrder,
-    refundOrder
+    refundOrder,
+    listItems
 };
